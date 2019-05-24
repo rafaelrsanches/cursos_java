b83f118cb9fc8dbcdc1e1443de5230b2bb6e28af
rafaelrsanches <rafael.rsanches96@gmail.com>
2019-05-24 20:08:10 -0300

    Hora do Sistema

---
+++ curso_em_video_guanabara_java_basico/HoraDoSistema.java
@@ -0,0 +1,21 @@
+
+import java.util.Date;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Rafael R. Sanches
+ */
+public class HoraDoSistema {
+    public static void main(String[] args) {
+        Date relogio = new Date();
+        
+        System.out.println("A hora do sistema é:");
+        System.out.println(relogio.toString());
+    }
+}
