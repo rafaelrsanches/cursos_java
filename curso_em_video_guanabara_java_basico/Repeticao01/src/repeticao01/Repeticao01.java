c46889fbcdf08ed0d912c3b215ebf4ffc01870e8
rafaelrsanches <rafael.rsanches96@gmail.com>
2019-06-18 20:47:51 -0300

    Estruturas de Repetição Parte 02

---
+++ curso_em_video_guanabara_java_basico/Repeticao01/src/repeticao01/Repeticao01.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repeticao01;
+
+/**
+ *
+ * @author Rafael R. Sanches
+ */
+public class Repeticao01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int cc = 0;
+        
+        do{
+            System.out.println("Cambalhota");
+            cc++;
+        }while (cc < 4);
+    }
+    
+}
