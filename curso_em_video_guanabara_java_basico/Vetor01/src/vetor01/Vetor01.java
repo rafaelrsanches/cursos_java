de46e5b04a9919e499bb1eb6ecec64e10a447f4a
rafaelrsanches <rafael.rsanches96@gmail.com>
2019-06-20 15:19:34 -0300

    Vetor 01

---
+++ curso_em_video_guanabara_java_basico/Vetor01/src/vetor01/Vetor01.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vetor01;
+
+/**
+ *
+ * @author Rafael R. Sanches
+ */
+public class Vetor01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n[] = {3, 2, 8, 7, 5, 4};
+        
+        for(int c = 0; c < n.length; c++){
+            System.out.println("Na posição " + c + " temos o valor " + n[c]);
+        }
+    }
+    
+}
