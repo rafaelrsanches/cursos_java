50b4254006b3d7e74d98af19433ce5c4a61bcd65
rafaelrsanches <rafael.rsanches96@gmail.com>
2019-06-20 15:50:51 -0300

    Vetor 03

---
+++ curso_em_video_guanabara_java_basico/Vetor03/src/vetor03/Vetor03.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vetor03;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author Rafael R. Sanches
+ */
+public class Vetor03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double v[] = {3.5, 2.75, 9, -4.5};
+        
+        Arrays.sort(v);
+        
+        for(double valor: v){
+            System.out.print(valor + " ");
+        }
+    }   
+}
