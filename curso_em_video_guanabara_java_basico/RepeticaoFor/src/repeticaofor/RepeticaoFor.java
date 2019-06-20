618beb4059567ae38ce764467ad2e89acf46f4ee
rafaelrsanches <rafael.rsanches96@gmail.com>
2019-06-19 22:42:53 -0300

    Estruturas de Repetição Parte 03

---
+++ curso_em_video_guanabara_java_basico/RepeticaoFor/src/repeticaofor/RepeticaoFor.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repeticaofor;
+
+/**
+ *
+ * @author Rafael R. Sanches
+ */
+public class RepeticaoFor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int cc = 0; cc <= 100; cc += 10){
+            System.out.println(cc);
+        }
+    }   
+}
